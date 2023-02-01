9692e9165dff1420c588657810b2558492dbec91
GangwalsGit <119483120+GangwalsGit@users.noreply.github.com>
2023-02-01 11:35:31 +0000

    Add files via upload

---
+++ React_janmejaya_imp/03-writing-more-complex-jsx-code/src/App.js
@@ -0,0 +1,16 @@
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <>
+    <h2>Hello</h2>
+    <div>
+      
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+    </>
+  );
+}
+
+export default App;
